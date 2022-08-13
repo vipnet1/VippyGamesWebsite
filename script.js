9cb0c92da5ebed2b7b43f4337b9669eeb80b4ab1
Timur Pichkhadze <timur.pichkhadze@gmail.com>
2022-08-13 17:48:41 +0300

    feat: .

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById('btnFTB').addEventListener('contextmenu', function(e){
+    e.preventDefault();
+  });
+
+document.getElementById('btnCM').addEventListener('contextmenu', function(e){
+    e.preventDefault();
+  });
